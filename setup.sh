34749fcab1a979899c3cfd48299b61ba8827d30a
Heiko Mathes <heiko.mathes@gmail.com>
2017-09-09 01:51:18 +0200

    :sparkles: add meta-command-examples and a setup-script

---
+++ setup.sh
@@ -0,0 +1,19 @@
+# checkout all repos in the correct branch
+meta git update
+meta exec "git checkout feature/2.0-cleanup"
+
+# install all necessary dependencies
+npm install
+npm install --no-save graphql@0.8.2 react@^15.4.0 react-dom@^15.4.0 react-relay@^0.9.3
+cd demo && npm install better-npm-run
+cd frontend && npm install
+cd ../backend && npm install
+cd ..
+
+# make the demo use the linked packages
+rm -rf frontend/node_modules/@process-engine-js
+rm -rf backend/node_modules/@process-engine-js
+cd ..
+
+# build all packages
+meta exec "npm run build"
