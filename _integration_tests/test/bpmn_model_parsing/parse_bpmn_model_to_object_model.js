278eab03e3eba1a6541bd606ebd4f88ab294f7b6
ElRaptorus <christian.werner@5minds.de>
2018-04-17 16:39:08 +0200

    Add assertions to bpmn model parser tests

--- _integration_tests/test/bpmn_model_parsing/parse_bpmn_model_to_object_model.js
+++ _integration_tests/test/bpmn_model_parsing/parse_bpmn_model_to_object_model.js
@@ -4,11 +4,13 @@ const should = require('should');
 const request = require('supertest');
 const fs = require('fs');
 const path = require('path');
-const inspect = require('util').inspect;
+
+const SequenceFlow = require('@process-engine/process_engine_contracts').Model.Types.SequenceFlow;
+const FlowNode = require('@process-engine/process_engine_contracts').Model.Base.FlowNode;
 
 const testSetup = require('../../application/test_setup');
 
-describe.only('Process-Engine   Parse BPMN Process into new object model', function () {
+describe('Process-Engine   Parse BPMN Process into new object model', function () {
   let httpBootstrapper;
   this.timeout(5000);
 
@@ -28,18 +30,45 @@ describe.only('Process-Engine   Parse BPMN Process into new object model', funct
     await httpBootstrapper.shutdown();
   });
 
-  it('Should successfully and correctly parse a BPMN diagram into an ObjectModel object', async() => {
+  it('Should successfully and correctly parse the BS Payone Sample BPMN into a ObjectModel structure', async() => {
 
-    const inspectOptions = {
-      showHidden: false,
-      depth: 9,
-      maxArrayLength: 100,
-    };
+    const result = await bpmnModelParser.parseXmlToObjectModel(sampleBpmnFile.bpmnXml);
 
-    // TODO: Add assertions
-    console.log(sampleBpmnFile);
-    const parsingResult = await bpmnModelParser.parseXmlToObjectModel(sampleBpmnFile.bpmnXml);
-    console.log(inspect(parsingResult, inspectOptions));
+    // Basic Definitions-Properties
+    should.exist(result.xmlns);
+    should.exist(result.collaboration);
+    should(result.id).be.equal('Definitions_1');
+    should(result.processes).be.an.instanceOf(Array);
+    should(result.processes.length).be.equal(1);
+
+    // Collaboration and Participants
+    const collaboration = result.collaboration;
+
+    should(collaboration.id).be.equal('Definitions_1');
+    should(collaboration.participants).be.an.instanceOf(Array);
+    should(collaboration.participants.length).be.equal(1);
+
+    const participant = collaboration.participants[0];
+
+    should(participant.id).be.equal('DemoProcess.Orchestration');
+    should(participant.name).be.equal('Akka.NET - DemoProcess');
+    should(participant.processReference).be.equal('TellProcess');
+
+    // Process-Properties
+    const process = result.processes[0];
+    should(process.id).be.equal('TellProcess');
+    should(process.name).be.equal('TellProcess');
+    should(process.isExecutable).be.equal(true);
+    should(process.flowNodes.length).be.equal(10);
+    should(process.sequenceFlows.length).be.equal(10);
+
+    process.flowNodes.forEach((item) => {
+      should(item).be.an.instanceof(FlowNode);
+    });
+
+    process.sequenceFlows.forEach((item) => {
+      should(item).be.an.instanceof(SequenceFlow);
+    });
   })
 
 });
