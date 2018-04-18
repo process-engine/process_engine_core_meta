afa42f3b28e59695f0b7c7680dd93529687d7562
ElRaptorus <christian.werner@5minds.de>
2018-04-18 17:40:52 +0200

    Add integrationtests for terminate end events

--- _integration_tests/test/error_boundary_event_test.js
+++ _integration_tests/test/error_boundary_event_test.js
@@ -1,7 +1,7 @@
 'use strict';
 
-const fixture = require('../application/dist/commonjs/process_engine_service_test_fixture')
 const should = require('should');
+const ProcessEngineServiceTestFixture = require('../dist/commonjs/process_engine_service_test_fixture').ProcessEngineServiceTestFixture;
 
 const testTimeoutInMS = 5000;
 
@@ -12,7 +12,7 @@ describe('Error Boundary Event execution', function () {
   this.timeout(testTimeoutInMS);
 
   before(async () => {
-    processEngineServiceFixture = new fixture.ProcessEngineServiceTestFixture(`${__dirname}/../application/bpmn/error_boundary_event_test.bpmn`);
+    processEngineServiceFixture = new ProcessEngineServiceTestFixture();
     await processEngineServiceFixture.setup();
   });
 
--- _integration_tests/test/terminate_end_event.js
+++ _integration_tests/test/terminate_end_event.js
@@ -1,31 +1,29 @@
 'use strict';
 
 const should = require('should');
-const setup = require('../application/test_setup');
+const ProcessEngineServiceTestFixture = require('../dist/commonjs/process_engine_service_test_fixture').ProcessEngineServiceTestFixture;
+
+const testTimeoutInMS = 5000;
 
 describe.only('Terminate End Event', function () {
 
-  let httpBootstrapper;
-  let dummyContext;
-  let processEngineService;
-  this.timeout(5000);
+  let processEngineServiceFixture;
+
+  this.timeout(testTimeoutInMS);
 
   before(async () => {
-    httpBootstrapper = await setup.initializeBootstrapper();
-    dummyContext = await setup.createContext();
-    processEngineService = await setup.resolveAsync('ProcessEngineService');
+    processEngineServiceFixture = new ProcessEngineServiceTestFixture();
+    await processEngineServiceFixture.setup();
   });
 
   after(async () => {
-    await httpBootstrapper.reset();
-    await httpBootstrapper.shutdown();
+    await processEngineServiceFixture.tearDown();
   });
 
   it(`should successfully terminate a process upon reaching a TerminateEndEvent.`, async () => {
 
     const processKey = 'terminate_end_event_sample';
-    const initialToken = {};
-    const result = await processEngineService.executeProcess(dummyContext, undefined, processKey, initialToken);
+    const result = await processEngineServiceFixture.executeProcess(processKey);
 
     const expectedResult = /terminated/i;
 
