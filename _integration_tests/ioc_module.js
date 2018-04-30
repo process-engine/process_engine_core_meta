90e34ed17d732244e6eef9740baf5fe10a14aece
ElRaptorus <christian.werner@5minds.de>
2018-04-30 16:53:42 +0200

    Enhance the tests for the ErrorBoundaryEvent with addtional test cases

--- _integration_tests/ioc_module.js
+++ _integration_tests/ioc_module.js
@@ -16,25 +16,27 @@ const registerInContainer = (container) => {
     'error_boundary_event_test',
     'generic_sample',
     'nested_xor',
-    'service_task_basic_test',
-    'service_task_exception_test',
     'parallel_gateway_test',
     'script_task_basic_test',
     'script_task_invalid_script',
     'script_task_throws_exception',
+    'service_task_basic_test',
+    'service_task_exception_test',
     'simple_xor_gateway_test',
     'subprocess_test',
     'terminate_end_event_sample',
     'xor_evaluate_script_result',
   ];
 
-  processes.map((processFilename) => registerProcess(processFilename, container));
+  processes.map((processFilename) => {
+    return registerProcess(processFilename, container);
+  });
 };
 
-const registerProcess = (processFilename, container) => {
+function registerProcess(processFilename, container) {
   const processFilePath = path.join(__dirname, 'bpmn', `${processFilename}.bpmn`);
   const processFile = fs.readFileSync(processFilePath, 'utf8');
- 
+
   return container.registerObject(processFilename, processFile)
     .setTag('bpmn_process', 'internal')
     .setTag('module', 'process_engine_meta')
