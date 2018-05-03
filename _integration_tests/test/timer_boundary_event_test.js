e140a299aef99e67c18a3a1cecf3c0407f4b88d3
robpal94 <robin.palkovits@5minds.de>
2018-05-03 14:46:37 +0200

    changes test to only against the past of the token instead of the time.

--- _integration_tests/test/timer_boundary_event_test.js
+++ _integration_tests/test/timer_boundary_event_test.js
@@ -3,7 +3,7 @@
 const should = require('should');
 const TestFixtureProvider = require('../dist/commonjs/test_fixture_provider').TestFixtureProvider;
 
-describe('Timer Boundary Event Tests', () => {
+describe.only('Timer Boundary Event Tests', () => {
 
   let testFixtureProvider;
 
@@ -16,14 +16,22 @@ describe('Timer Boundary Event Tests', () => {
     await testFixtureProvider.tearDown();
   });
 
-  it('should interrupt a service task after two seconds.', async () => {
+  it('should interrupt a service task after two seconds and not interrupt a task service, that finishes before the timer was over.', async () => {
     const processKey = 'timer_boundary_event_base_test';
 
-    const expectedTimeElapsedTask1 = 2;
-
-    const expectedTimeElapsedTask2 = 2;
-
-    const expectedKeysInHistory = ['Task1', 'TimerBoundary1', 'XORJoin1', 'Task3', 'Task4', 'Task5', 'TimerBoundary2', 'XORJoin2', 'Task6'];
+    // Expected Token Object
+    const expectedToken = {
+      current: 3,
+      history: {
+        Task1: 1,
+        TimerBoudary1: 1,
+        Task2: 2,
+        XORJoin1: 2,
+        Task4: 2,
+        Task5: 3,
+        XORJoin2: 3,
+      },
+    };
 
     // Execute the process
     const result = await testFixtureProvider.executeProcess(processKey);
@@ -31,17 +39,13 @@ describe('Timer Boundary Event Tests', () => {
     // Check, if the resulting token is not undefined.
     should(result).not.be.undefined();
 
+    // Check, if the resulting token is an object
+    should(result).be.Object();
+
     // Check, if the resulting token is not empty
     result.should.not.be.empty();
 
-    // Check, if the result contains the right keys
-    result.should.have.keys(expectedKeysInHistory);
-
-    // Check, if the ellapsed time after the first service task is the expected.
-    result.XORJoin1.should.be.equal(expectedTimeElapsedTask1);
-
-    // Check, if the ellapsed time after the second service task ist the expected one.
-    result.XORJoin2.should.be.equal(expectedTimeElapsedTask2);
-
+    // Check the token, that was returned.
+    result.should.be.eql(expectedToken);
   });
 });
