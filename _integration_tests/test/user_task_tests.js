c2da47d1c28a660dfa91cc1c0730a0be856a09d8
robpal94 <robin.palkovits@5minds.de>
2018-05-08 15:27:44 +0200

    I can now fetch started user tasks and finish them with a given value.

--- _integration_tests/test/user_task_tests.js
+++ _integration_tests/test/user_task_tests.js
@@ -2,6 +2,7 @@
 
 const should = require('should');
 const TestFixtureProvider = require('../dist/commonjs/test_fixture_provider').TestFixtureProvider;
+const startCallbackType = require('@process-engine/consumer_api_contracts').StartCallbackType;
 
 describe.only('User Tasks', () => {
   let testFixtureProvider;
@@ -21,12 +22,29 @@ describe.only('User Tasks', () => {
 
     const processKey = 'user_tasks_base_test';
 
-    console.log(consumerContext);
+    const inputValues = {
+      input_values: {},
+    };
 
+    const callbackType = startCallbackType.CallbackOnProcessInstanceCreated;
 
-    const correlationId = await testFixtureProvider.consumerApiService.startProcessInstance(consumerContext, processKey, 'StartEvent_1');
+    const correlationId = await testFixtureProvider.consumerApiService.startProcessInstance(
+      consumerContext, processKey, 'StartEvent_1', inputValues, callbackType);
 
-    console.log(correlationId);
+    const userTask = await testFixtureProvider.consumerApiService.getUserTasksForCorrelation(consumerContext, correlationId.correlation_id);
+
+    const userTaskResult = {
+      form_fields: {
+        Sample_Form_Field: 'Hello',
+      },
+    };
+
+    console.log(userTask);
+
+    const utres = await testFixtureProvider.consumerApiService.finishUserTask(consumerContext,
+      processKey, correlationId.correlation_id, userTask.user_tasks[0].key, userTaskResult);
+
+    console.log(utres);
 
   });
 });
