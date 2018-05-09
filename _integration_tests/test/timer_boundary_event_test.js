d2487f6a731b0767db4c9cf104c96d82e76954b1
robpal94 <robin.palkovits@5minds.de>
2018-05-09 15:10:14 +0200

    :recycle: refactor the timer event test

--- _integration_tests/test/timer_boundary_event_test.js
+++ _integration_tests/test/timer_boundary_event_test.js
@@ -3,9 +3,12 @@
 const should = require('should');
 const TestFixtureProvider = require('../dist/commonjs/test_fixture_provider').TestFixtureProvider;
 
-describe('Timer Boundary Event - ', () => {
+describe.only('Timer Boundary Event - ', () => {
 
   let testFixtureProvider;
+
+  // Every Test uses the same process model.
+  const processKey = 'boundary_event_timer_test';
 
   before(async () => {
     testFixtureProvider = new TestFixtureProvider();
@@ -16,28 +19,60 @@ describe('Timer Boundary Event - ', () => {
     await testFixtureProvider.tearDown();
   });
 
-  it('should interrupt a service task after two seconds and not interrupt a service task, that finishes before the timer was over.', async () => {
-    const processKey = 'boundary_event_timer_test';
+  it('should interrupt a service task after two seconds', async () => {
+
+    // Initial token object
+    const initialToken = {
+      interrupt_task: true,
+    };
 
     // Expected Token Object
     const expectedToken = {
-      current: 3,
+      current: 2,
       history: {
-        StartEvent_1: {},
-        Task1: 1,
-        TimerBoudary1: 1,
-        Task2: 2,
+        StartEvent_1: initialToken,
+        XORSplit1: initialToken,
+        ITask1: 1,
+        ITimerBoundary1: 1,
+        ITask2: 2,
+        IXORJoin1: 2,
         XORJoin1: 2,
-        Task4: 2,
-        Task5: 3,
-        XORJoin2: 3,
       },
     };
 
     // Execute the process
-    const result = await testFixtureProvider.executeProcess(processKey);
+    const result = await testFixtureProvider.executeProcess(processKey, initialToken);
 
     // Check the token, that was returned.
     should(result).be.eql(expectedToken);
+  });
+
+  it('should not interrupt a service task that finishes, before the timespan of the timer boundary event is over', async () => {
+
+    // Initial token object
+    const initialToken = {
+      interrupt_task: false,
+    };
+
+    // Expected Token object
+    const expectedToken = {
+      current: 2,
+      history: {
+        StartEvent_1: initialToken,
+        XORSplit1: initialToken,
+        NITask1: 1,
+        NITask2: 1,
+        NITask3: 2,
+        NIXORJoin1: 2,
+        XORJoin1: 2,
+      },
+    };
+
+    // Execute the process
+    const result = await testFixtureProvider.executeProcess(processKey, initialToken);
+
+    // Check the returned token
+    should(result).be.eql(expectedToken);
+
   });
 });
