4dba4bebf032ad77a4f65333b80c39b35291a4b9
ElRaptorus <christian.werner@5minds.de>
2018-05-23 10:53:24 +0200

    Fix some typos

--- _integration_tests/test/conditional_boundary_event.js
+++ _integration_tests/test/conditional_boundary_event.js
@@ -3,22 +3,25 @@
 const should = require('should');
 const TestFixtureProvider = require('../dist/commonjs/test_fixture_provider').TestFixtureProvider;
 
-describe('Conditional Boundary Event', () => {
+describe.skip('Conditional Boundary Event - ', () => {
   let testFixtureProvider;
 
   before(async () => {
     testFixtureProvider = new TestFixtureProvider();
     await testFixtureProvider.initializeAndStart();
 
-    const testProcessDefFileName = ['boundary_event_conditional.bpmn'];
-    await testFixtureProvider.loadProcessesFromBPMNFiles(testProcessDefFileName);
+    // TODO: The import is currently broken (existing processes are duplicated, not overwritten).
+    // Until this is fixed, use the "classic" ioc registration
+    //
+    // const testProcessDefFileName = ['boundary_event_conditional.bpmn'];
+    // await testFixtureProvider.loadProcessesFromBPMNFiles(testProcessDefFileName);
   });
 
   after(async () => {
     await testFixtureProvider.tearDown();
   });
 
-  it.skip('should return "lesser" if the initial token is lesser than or equal to four.', async () => {
+  it('should return "lesser", if the initial token is lesser than or equal 4.', async () => {
     const processKey = 'boundary_event_conditional';
 
     const result = await testFixtureProvider.executeProcess(processKey, 4);
@@ -27,7 +30,7 @@ describe('Conditional Boundary Event', () => {
     should(result.current).be.eql('lesser');
   });
 
-  it.skip('should return "greater" if the initial token is greater than or equal to six.', async () => {
+  it('should return "greater", if the initial token is greater than or equal 6.', async () => {
     const processKey = 'boundary_event_conditional';
 
     const result = await testFixtureProvider.executeProcess(processKey, 6);
