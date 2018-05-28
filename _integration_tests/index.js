5105e729ea608bd1be4105e939290dd13f7b952e
5ebastianMeier <sebastian.meier@5minds.de>
2018-05-28 09:53:54 +0200

    add consumer api core package to test setup

--- _integration_tests/index.js
+++ _integration_tests/index.js
@@ -33,6 +33,7 @@ const iocModuleNames = [
   '@essential-projects/routing',
   '@essential-projects/timing',
   '@essential-projects/validation',
+  '@process-engine/consumer_api_core',
   '@process-engine/process_engine',
   '@process-engine/process_engine_http',
   '@process-engine/process_repository',
@@ -60,14 +61,14 @@ async function start() {
     for (const iocModule of iocModules) {
       iocModule.registerInContainer(container);
     }
-  
+
     container.validateDependencies();
-    
+
     const appPath = path.resolve(__dirname);
     const bootstrapper = await container.resolveAsync('AppBootstrapper', [appPath]);
 
     logger.info('Bootstrapper started.');
-  
+
     await bootstrapper.start();
   } catch (error) {
     logger.error('Failed to start bootstrapper!', error);
