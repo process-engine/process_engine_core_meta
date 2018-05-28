768023666b2c46600ed096b449b5539849d8d9c6
ElRaptorus <christian.werner@5minds.de>
2018-05-28 09:57:07 +0200

    Remove handler dump from test fixture provider

--- _integration_tests/src/test_fixture_provider.ts
+++ _integration_tests/src/test_fixture_provider.ts
@@ -2,7 +2,6 @@ import * as fs from 'fs';
 // tslint:disable-next-line:import-blacklist
 import * as _ from 'lodash';
 import * as path from 'path';
-import * as wtf from 'wtfnode';
 
 import {InvocationContainer} from 'addict-ioc';
 import {Logger} from 'loggerhythm';
@@ -166,8 +165,6 @@ export class TestFixtureProvider {
   public async tearDown(): Promise<void> {
     await this.bootstrapper.reset();
     await this.bootstrapper.shutdown();
-    console.log('DISPOSAL COMPLETE');
-    wtf.dump();
   }
 
   private async initializeBootstrapper(): Promise<void> {
