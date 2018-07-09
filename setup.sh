82c52e13b3e6732bc91726387752c0368c077006
ElRaptorus <christian.werner@5minds.de>
2018-07-09 15:40:40 +0200

    Update build scripts to remove schema building

--- setup.sh
+++ setup.sh
@@ -23,8 +23,8 @@ fi
 rm -rf node_modules/@types/jasmine
 
 # build all packages and schemas
-meta exec "npm run build && npm run build-schemas" --exclude process_engine_meta,skeleton,documentation,bpmn-io_custom-bundle,bpmn-studio,tslint-config,_integration_tests
-meta exec "npm run build" --include-only skeleton,bpmn-io_custom-bundle,bpmn-studio,_integration_tests
+meta exec "npm run build && npm run build-schemas" --include-only process_engine_contracts
+meta exec "npm run build" --exclude process_engine_meta,skeleton,documentation,identity_server
 
 # create a database
 cd skeleton/database
