486ddb3c3fe6a13604b5a527a318315497abb520
ElRaptorus <christian.werner@5minds.de>
2018-09-05 10:07:47 +0200

    :recycle: Refactor migration scripts to accomondate correlations

--- _integration_tests/sequelize/migrations/correlations/template.js
+++ _integration_tests/sequelize/migrations/correlations/template.js
@@ -4,7 +4,7 @@
 // This file is required, because electron will not unpack empty folders from the app.asar archive.
 // But we need this folder, so that Sequelize will be able to run correctly.
 // Apparently, keeping a .gitkeep in this folder is insufficient to ensure this.
-// We can replace this file, once a migration is required for the ProcessModels, but until then,
+// We can replace this file, once a migration is required for the Correlations, but until then,
 // this dummy file has to stay.
 module.exports = {
   up: async (queryInterface, Sequelize) => {
