7ce69121685b9301e631c8b2ee0869ea5879fe2c
ElRaptorus <christian.werner@5minds.de>
2018-11-29 12:31:56 +0100

    :recycle: Rename initial correlation migration

--- _integration_tests/sequelize/migrations/correlation/0.0.0-1.0.0_initialize_correlations.js
+++ _integration_tests/sequelize/migrations/correlation/0.0.0-1.0.0_initialize_correlations.js
@@ -26,11 +26,6 @@ module.exports = {
     console.log('Creating Correlations table');
 
     return queryInterface.createTable('Correlations', {
-      id: {
-        type: Sequelize.UUID,
-        primaryKey: true,
-        defaultValue: Sequelize.UUIDV4,
-      },
       correlationId: {
         type: Sequelize.STRING,
         allowNull: false,
