347f3409c54368fd0f5d7d5b7ff9dc8bcbeb3306
ElRaptorus <christian.werner@5minds.de>
2018-12-04 09:58:24 +0100

    :white_check_mark: Add test for parsing ProcessModels with empty lanes

--- _integration_tests/test/parse_bpmn_model_to_object_model.js
+++ _integration_tests/test/parse_bpmn_model_to_object_model.js
@@ -56,4 +56,41 @@ describe('Process-Engine   Parse BPMN Process into new object model', () => {
     should(process.sequenceFlows.length).be.equal(10);
   });
 
+  it('should successfully parse a ProcessModel which contains an empty lane', async () => {
+
+    const sampleProcessName = 'empty_lane_test';
+
+    const sampleBpmnFile = testFixtureProvider.readProcessModelFile(sampleProcessName);
+
+    const result = await bpmnModelParser.parseXmlToObjectModel(sampleBpmnFile);
+
+    // Basic Definitions-Properties
+    should.exist(result.xmlns);
+    should.exist(result.collaboration);
+    should(result.id).be.equal('Definition_1');
+    should(result.processes).be.an.instanceOf(Array);
+    should(result.processes.length).be.equal(1);
+
+    // Collaboration and Participants
+    const collaboration = result.collaboration;
+
+    should(collaboration.id).be.equal('Definition_1');
+    should(collaboration.participants).be.an.instanceOf(Array);
+    should(collaboration.participants.length).be.equal(1);
+
+    const participant = collaboration.participants[0];
+
+    should(participant.id).be.equal('Participant_0px403d');
+    should(participant.name).be.equal('empty_lane_test');
+    should(participant.processReference).be.equal('empty_lane_test');
+
+    // Process-Properties
+    const process = result.processes[0];
+    should(process.id).be.equal('empty_lane_test');
+    should(process.name).be.equal('empty_lane_test');
+    should(process.isExecutable).be.equal(false);
+    should(process.flowNodes.length).be.equal(2);
+    should(process.sequenceFlows.length).be.equal(1);
+  });
+
 });
