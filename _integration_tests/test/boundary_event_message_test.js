081441a640dc91c9cb06649f332947e70643b2b7
ElRaptorus <christian.werner@5minds.de>
2019-02-12 14:11:51 +0100

    :white_check_mark: Add tests for non-interrupting BoundaryEvents

--- _integration_tests/test/boundary_event_message_test.js
+++ _integration_tests/test/boundary_event_message_test.js
@@ -9,6 +9,8 @@ describe('Message Boundary Event - ', () => {
 
   let eventAggregator;
   let processInstanceHandler;
+
+  let defaultIdentity;
   let testFixtureProvider;
 
   const processModelId = 'boundary_event_message_test';
@@ -19,6 +21,8 @@ describe('Message Boundary Event - ', () => {
     testFixtureProvider = new TestFixtureProvider();
     await testFixtureProvider.initializeAndStart();
 
+    defaultIdentity = testFixtureProvider.identities.defaultUser;
+
     eventAggregator = await testFixtureProvider.resolveAsync('EventAggregator');
     processInstanceHandler = new ProcessInstanceHandler(testFixtureProvider);
     await testFixtureProvider.importProcessFiles([processModelId]);
@@ -28,7 +32,7 @@ describe('Message Boundary Event - ', () => {
     await testFixtureProvider.tearDown();
   });
 
-  it('should interrupt the UserTask when a message arrives', async () => {
+  it('should interrupt the task when a message arrives at an interrupting BoundaryEvent', async () => {
 
     const expectedResult = /message received/i;
 
@@ -46,6 +50,46 @@ describe('Message Boundary Event - ', () => {
 
       const triggerMessageEventName = '/processengine/process/message/Message1234';
       eventAggregator.publish(triggerMessageEventName, {});
+    });
+  });
+
+  it('should no longer list the task as active, after it was interrupted', async () => {
+
+    const userTaskList = await testFixtureProvider
+      .consumerApiService
+      .getUserTasksForProcessModelInCorrelation(defaultIdentity, processModelId, correlationId);
+
+    should(userTaskList.userTasks).be.an.Array();
+    should(userTaskList.userTasks).be.length(0, 'The UserTask was not interrupted properly!');
+  });
+
+  it('should not interrupt the task when a message arrives at a non-interrupting BoundaryEvent', async () => {
+
+    const startEventId2 = 'StartEvent_2';
+    testFixtureProvider.executeProcess(processModelId, startEventId2, correlationId);
+    await processInstanceHandler.waitForProcessInstanceToReachSuspendedTask(correlationId, processModelId);
+
+    await new Promise(async (resolve) => {
+
+      processInstanceHandler.waitForProcessInstanceToEnd(correlationId, processModelId, resolve);
+
+      const triggerMessageEventName = '/processengine/process/message/Message1234';
+      eventAggregator.publish(triggerMessageEventName, {});
+
+      await new Promise((cb) => { setTimeout(cb, 1000); });
+
+      const userTaskList = await testFixtureProvider
+        .consumerApiService
+        .getUserTasksForProcessModelInCorrelation(defaultIdentity, processModelId, correlationId);
+
+      should(userTaskList.userTasks).be.an.Array();
+      should(userTaskList.userTasks.length).be.greaterThan(0, 'The UserTask was interrupted!');
+
+      const userTask = userTaskList.userTasks[0];
+
+      await testFixtureProvider
+        .consumerApiService
+        .finishUserTask(defaultIdentity, userTask.processInstanceId, userTask.correlationId, userTask.flowNodeInstanceId, {});
     });
   });
 });
--- _integration_tests/test/boundary_event_signal_test.js
+++ _integration_tests/test/boundary_event_signal_test.js
@@ -9,6 +9,8 @@ describe('Signal Boundary Event - ', () => {
 
   let eventAggregator;
   let processInstanceHandler;
+
+  let defaultIdentity;
   let testFixtureProvider;
 
   const processModelId = 'boundary_event_signal_test';
@@ -19,6 +21,8 @@ describe('Signal Boundary Event - ', () => {
     testFixtureProvider = new TestFixtureProvider();
     await testFixtureProvider.initializeAndStart();
 
+    defaultIdentity = testFixtureProvider.identities.defaultUser;
+
     eventAggregator = await testFixtureProvider.resolveAsync('EventAggregator');
     processInstanceHandler = new ProcessInstanceHandler(testFixtureProvider);
     await testFixtureProvider.importProcessFiles([processModelId]);
@@ -28,7 +32,7 @@ describe('Signal Boundary Event - ', () => {
     await testFixtureProvider.tearDown();
   });
 
-  it('should interrupt the UserTask when a signal arrives', async () => {
+  it('should interrupt the task when a signal arrives at an interrupting BoundaryEvent', async () => {
 
     const expectedResult = /signal received/i;
 
@@ -46,6 +50,46 @@ describe('Signal Boundary Event - ', () => {
 
       const triggerSignalEventName = '/processengine/process/signal/Signal1234';
       eventAggregator.publish(triggerSignalEventName, {});
+    });
+  });
+
+  it('should no longer list the task as active, after it was interrupted', async () => {
+
+    const userTaskList = await testFixtureProvider
+      .consumerApiService
+      .getUserTasksForProcessModelInCorrelation(defaultIdentity, processModelId, correlationId);
+
+    should(userTaskList.userTasks).be.an.Array();
+    should(userTaskList.userTasks).be.length(0, 'The UserTask was not interrupted properly!');
+  });
+
+  it('should not interrupt the task when a message arrives at a non-interrupting BoundaryEvent', async () => {
+
+    const startEventId2 = 'StartEvent_2';
+    testFixtureProvider.executeProcess(processModelId, startEventId2, correlationId);
+    await processInstanceHandler.waitForProcessInstanceToReachSuspendedTask(correlationId, processModelId);
+
+    await new Promise(async (resolve) => {
+
+      processInstanceHandler.waitForProcessInstanceToEnd(correlationId, processModelId, resolve);
+
+      const triggerMessageEventName = '/processengine/process/signal/Signal1234';
+      eventAggregator.publish(triggerMessageEventName, {});
+
+      await new Promise((cb) => { setTimeout(cb, 1000); });
+
+      const userTaskList = await testFixtureProvider
+        .consumerApiService
+        .getUserTasksForProcessModelInCorrelation(defaultIdentity, processModelId, correlationId);
+
+      should(userTaskList.userTasks).be.an.Array();
+      should(userTaskList.userTasks.length).be.greaterThan(0, 'The UserTask was interrupted!');
+
+      const userTask = userTaskList.userTasks[0];
+
+      await testFixtureProvider
+        .consumerApiService
+        .finishUserTask(defaultIdentity, userTask.processInstanceId, userTask.correlationId, userTask.flowNodeInstanceId, {});
     });
   });
 });
