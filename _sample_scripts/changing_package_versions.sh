0f5c338d75b9ed303777ea949f022aec9ca7ccd4
ElRaptorus <christian.werner@5minds.de>
2019-06-24 09:11:02 +0200

    :fire: Remove sample scripts These are now located in the Contribution Guidelines repository under the name 'tooling'

--- _sample_scripts/changing_package_versions.sh
+++
@@ -1,8 +0,0 @@
-# adding a ^ to all essential-projects-versions that don't have it
-meta exec --exclude documentation,skeleton,process_engine_meta "perl -pi -e 's#\\\"\\@essential-projects\\/([^\\\"]+)\\\":\\s+\\\"([^\\\^][^\\\"]+)\\\"#\"\\\"\\@essential-projects\\/\" . \$1 . \"\\\": \\\"^\" . \$2 . \"\\\"\"#ge' **/package.json package.json"
-
-# adding a ^ to all process-engine-versions that don't have it
-meta exec --exclude documentation,skeleton,process_engine_meta "perl -pi -e 's#\\\"\\@process-engine\\/([^\\\"]+)\\\":\\s+\\\"([^\\\^][^\\\"]+)\\\"#\"\\\"\\@process-engine\\/\" . \$1 . \"\\\": \\\"^\" . \$2 . \"\\\"\"#ge' **/package.json package.json"
-
-# raising all rc-versions by 1
-meta exec "perl -pi -e 's/\\s+\"version\":\\s+\"(\\d+).\\d+.\\d+-rc(\\d)\"(.+)/\"  \\\"version\\\": \\\"\" . (\$1) . \".0.0-rc\" . (\$2+1) . \"\\\"\" . \$3/ge' package.json"
--- _sample_scripts/remove_old_tags.sh
+++
@@ -1,4 +0,0 @@
-# For process_engine_meta
-ruby remove_old_tags.rb
-# For all other repos, assuming this is run from the process_engine_meta folder
-meta exec "ruby ../_sample_scripts/remove_old_tags.rb" --exclude process_engine_meta
