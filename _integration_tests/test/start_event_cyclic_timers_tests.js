eeb0fd32eb31d6e1738a52973364945f623b3774
ElRaptorus <christian.werner@5minds.de>
2019-06-27 16:46:12 +0200

    :construction: :white_check_mark: Add test for Cyclic TimerStartEvents

---
+++ _integration_tests/test/start_event_cyclic_timers_tests.js
@@ -0,0 +1,44 @@
+'use strict';
+
+const moment = require('moment');
+const should = require('should');
+const uuid = require('node-uuid');
+
+const {ProcessInstanceHandler, TestFixtureProvider} = require('../dist/commonjs');
+
+describe.only('Start Events - ', () => {
+
+  let cronjobService;
+  let testFixtureProvider;
+  let processInstanceHandler;
+
+  const processModelId = 'start_events_with_cyclic_timers_test';
+
+  before(async () => {
+    testFixtureProvider = new TestFixtureProvider();
+    await testFixtureProvider.initializeAndStart();
+
+    await testFixtureProvider.importProcessFiles([processModelId]);
+
+    //cronjobService = await testFixtureProvider.resolveAsync('CronjobService');
+    processInstanceHandler = new ProcessInstanceHandler(testFixtureProvider);
+
+    // await cronjobService.start();
+  });
+
+  after(async () => {
+    // await cronjobService.stop();
+    await testFixtureProvider.tearDown();
+  });
+
+  it('should be able to start a process with a cyclic TimerStartEvent event manually.', async () => {
+
+    const messageStartEventId = 'TimerStartEvent_1';
+
+    const result = await testFixtureProvider.executeProcess(processModelId, messageStartEventId);
+
+    const expectedResult = /success/i;
+    should(result).have.property('currentToken');
+    should(result.currentToken).be.match(expectedResult);
+  });
+});
