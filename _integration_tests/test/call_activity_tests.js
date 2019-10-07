fed19dce1de6cd01fd09431785b2a474a04f21e7
ElRaptorus <christian.werner@5minds.de>
2019-10-07 12:34:07 +0200

    :white_check_mark: Add tests for CallActivity configurations

--- _integration_tests/test/call_activity_tests.js
+++ _integration_tests/test/call_activity_tests.js
@@ -3,14 +3,11 @@
 const should = require('should');
 const uuid = require('node-uuid');
 
-const {ProcessInstanceHandler, TestFixtureProvider} = require('../dist/commonjs');
+const {TestFixtureProvider} = require('../dist/commonjs');
 
 describe('Call activity tests', () => {
 
-  let processInstanceHandler;
   let testFixtureProvider;
-
-  let finishedCorrelationId;
 
   before(async () => {
     testFixtureProvider = new TestFixtureProvider();
@@ -25,14 +22,13 @@ describe('Call activity tests', () => {
     ];
 
     await testFixtureProvider.importProcessFiles(processDefFileList);
-    processInstanceHandler = new ProcessInstanceHandler(testFixtureProvider);
   });
 
   after(async () => {
     await testFixtureProvider.tearDown();
   });
 
-  it('should successfully execute a CallActivity and process the result', async () => {
+  it('should successfully execute a CallActivity, using default values for payload and StartEventId', async () => {
 
     const processModelId = 'call_activity_test';
     const correlationId = uuid.v4();
@@ -40,36 +36,62 @@ describe('Call activity tests', () => {
       operation: 'basic_test',
     };
 
-    let finalResult;
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_1', correlationId, initialToken);
 
-    await new Promise(async (resolve, reject) => {
-      const startResult = await processInstanceHandler.startProcessInstanceAndReturnResult(processModelId, correlationId, initialToken);
+    const finalToken = result.currentToken;
 
-      processInstanceHandler.waitForProcessWithInstanceIdToEnd(startResult.processInstanceId, (message) => {
-        finalResult = message.currentToken;
-        resolve();
-      });
-    });
-
-    finishedCorrelationId = correlationId;
-
-    should(finalResult.result).be.eql(2, `Expected a result of 2, but got ${finalResult.result}`);
-    should(finalResult.endEventId).be.eql('EndEvent_00hkafj', `Unexpected EndEventId: ${finalResult.endEventId}`);
-    should(finalResult.endEventName).be.eql('End', `Unexpected EndEventName: ${finalResult.endEventName}`);
+    should(finalToken.result).be.eql(2, `Expected a result of 2, but got ${finalToken.result}`);
+    should(finalToken.endEventId).be.eql('EndEvent_00hkafj', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('EndEvent 1', `Unexpected EndEventName: ${finalToken.endEventName}`);
   });
 
-  it('should ensure that the state of the executed CallActivity was set to finished', async () => {
+  it('should start the CallActivity from the given StartEventId', async () => {
 
-    const correlationRepository = await testFixtureProvider.resolveAsync('CorrelationRepository');
+    const processModelId = 'call_activity_test';
+    const correlationId = uuid.v4();
+    const initialToken = {};
 
-    const entries = await correlationRepository.getByCorrelationId(finishedCorrelationId);
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_TestAttachedStartEventId', correlationId, initialToken);
 
-    should(entries.length).be.equal(2);
+    const finalToken = result.currentToken;
 
-    for (const entry of entries) {
-      const assertionErrorMessage = `ProcessInstance ${entry.processInstanceId} in Correlation ${finishedCorrelationId} was not finished!`;
-      should(entry.state).be.equal('finished', assertionErrorMessage);
+    should(finalToken.result).be.eql({}, `Expected an empty result, but got ${finalToken.result}`);
+    should(finalToken.endEventId).be.eql('EndEvent_1obbzk2', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('EndEvent 2', `Unexpected EndEventName: ${finalToken.endEventName}`);
+  });
+
+  it('should pass the configured payload to the CallActivity', async () => {
+
+    const processModelId = 'call_activity_test';
+    const correlationId = uuid.v4();
+    const initialToken = {};
+
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_TestAttachedPayload', correlationId, initialToken);
+
+    const finalToken = result.currentToken;
+
+    should(finalToken.result).be.eql(12345, `Unexpected result: ${finalToken.result}`);
+    should(finalToken.endEventId).be.eql('EndEvent_1obbzk2', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('EndEvent 2', `Unexpected EndEventName: ${finalToken.endEventName}`);
+  });
+
+  it('should be able to use a payload that uses token expressions', async () => {
+
+    const processModelId = 'call_activity_test';
+    const correlationId = uuid.v4();
+    const initialToken = 'Hello World';
+
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_TestTokenizedPayload', correlationId, initialToken);
+
+    const finalToken = result.currentToken;
+
+    const expectedResult = {
+      bla: 'Hello World'
     }
+
+    should(finalToken.result).be.eql(expectedResult, `Unexpected result: ${finalToken.result}`);
+    should(finalToken.endEventId).be.eql('EndEvent_1obbzk2', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('EndEvent 2', `Unexpected EndEventName: ${finalToken.endEventName}`);
   });
 
   it('should successfully handle nested CallActivities', async () => {
@@ -80,20 +102,13 @@ describe('Call activity tests', () => {
       operation: 'nested_test',
     };
 
-    let finalResult;
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_1', correlationId, initialToken);
 
-    await new Promise(async (resolve, reject) => {
-      const startResult = await processInstanceHandler.startProcessInstanceAndReturnResult(processModelId, correlationId, initialToken);
+    const finalToken = result.currentToken;
 
-      processInstanceHandler.waitForProcessWithInstanceIdToEnd(startResult.processInstanceId, (message) => {
-        finalResult = message.currentToken;
-        resolve();
-      });
-    });
-
-    should(finalResult.result).be.eql(4, `Expected a result of 4, but got ${finalResult.result}`);
-    should(finalResult.endEventId).be.eql('EndEvent_1rlvyot', `Unexpected EndEventId: ${finalResult.endEventId}`);
-    should(finalResult.endEventName).be.eql('End', `Unexpected EndEventName: ${finalResult.endEventName}`);
+    should(finalToken.result).be.eql(4, `Expected a result of 4, but got ${finalToken.result}`);
+    should(finalToken.endEventId).be.eql('EndEvent_1rlvyot', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('End', `Unexpected EndEventName: ${finalToken.endEventName}`);
   });
 
   it('should successfully handle an error from a called CallActivity', async () => {
@@ -104,20 +119,13 @@ describe('Call activity tests', () => {
       handle_exception: true,
     };
 
-    let finalResult;
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_1', correlationId, initialToken);
 
-    await new Promise(async (resolve, reject) => {
-      const startResult = await processInstanceHandler.startProcessInstanceAndReturnResult(processModelId, correlationId, initialToken);
+    const finalToken = result.currentToken;
 
-      processInstanceHandler.waitForProcessWithInstanceIdToEnd(startResult.processInstanceId, (message) => {
-        finalResult = message.currentToken;
-        resolve();
-      });
-    });
-
-    should(finalResult.result).be.match(/error caught by subprocess/i);
-    should(finalResult.endEventId).be.eql('EndEvent_1udbard', `Unexpected EndEventId: ${finalResult.endEventId}`);
-    should(finalResult.endEventName).be.eql('End', `Unexpected EndEventName: ${finalResult.endEventName}`);
+    should(finalToken.result).be.match(/error caught by subprocess/i);
+    should(finalToken.endEventId).be.eql('EndEvent_1udbard', `Unexpected EndEventId: ${finalToken.endEventId}`);
+    should(finalToken.endEventName).be.eql('End', `Unexpected EndEventName: ${finalToken.endEventName}`);
   });
 
   it('should successfully handle a CallActivity that handled an error internally', async () => {
@@ -128,17 +136,41 @@ describe('Call activity tests', () => {
       handle_exception: false,
     };
 
-    let finalResult;
+    const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_1', correlationId, initialToken);
 
-    await new Promise(async (resolve, reject) => {
-      const startResult = await processInstanceHandler.startProcessInstanceAndReturnResult(processModelId, correlationId, initialToken);
+    should(result.currentToken).be.match(/error caught by main process boundary event/i);
+  });
 
-      processInstanceHandler.waitForProcessWithInstanceIdToEnd(startResult.processInstanceId, (message) => {
-        finalResult = message.currentToken;
-        resolve();
-      });
-    });
+  it('should throw an error, if the attached StartEventId does not exist on the CallActivity', async () => {
+    try {
+      const processModelId = 'call_activity_test';
+      const correlationId = uuid.v4();
+      const initialToken = {};
 
-    should(finalResult).be.match(/error caught by main process boundary event/i);
+      const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_TestInvalidStartEventId', correlationId, initialToken);
+      should.fail(result, undefined, 'This should have failed, because the CallActivity does not have the configured start event!');
+    } catch (error) {
+      const expectedErrorMessage = /no matching startevent/i
+      const expectedErrorCode = 404;
+      should(error.message).be.match(expectedErrorMessage);
+      should(error.code).be.equal(expectedErrorCode);
+      should(error.additionalInformation).have.a.property('configuredStartEventId');
+      should(error.additionalInformation.configuredStartEventId).be.equal('InvalidStartEventID');
+    }
+  });
+
+  it('should throw an error, if the configured payload is invalid', async () => {
+    try {
+      const processModelId = 'call_activity_test';
+      const correlationId = uuid.v4();
+      const initialToken = {};
+
+      const result = await testFixtureProvider.executeProcess(processModelId, 'StartEvent_TestInvalidPayload', correlationId, initialToken);
+    } catch (error) {
+      const expectedErrorMessage = /payload configuration.*?invalid/i
+      const expectedErrorCode = 500;
+      should(error.message).be.match(expectedErrorMessage);
+      should(error.code).be.equal(expectedErrorCode);
+    }
   });
 });
